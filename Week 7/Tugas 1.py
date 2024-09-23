4406296150cdc0a36ddeeca1949dd35689b0a7ef
NaufaldaSinatriya <naufaldasinatriyaindrawan@mail.ugm.ac.id>
2024-09-24 02:09:08 +0700

    Add files via upload

---
+++ Week 7/Tugas 1.py
@@ -0,0 +1,5 @@
+my_dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+
+print("my_dict['Name']: ", my_dict['Name'])
+print("my_dict['Age']: ", my_dict['Age'])
+print("my_dict['Class']: ", my_dict['Class'])
